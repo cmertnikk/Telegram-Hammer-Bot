e2ddbb5516b680cdeed48d9d76b1b6c16ad2886c
cmertnikk <bbbhh6403@gmail.com>
2025-05-11 22:37:45 +0300

    Update bot-logger.py

--- bot-logger.py
+++ bot-logger.py
@@ -4,7 +4,7 @@ from telegraph import Telegraph, TelegraphException
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 TOKEN = ''
-bot = telebot.TeleBot('ВАШ ТОКЕН')
+bot = telebot.TeleBot(8159731569:AAFUD6Rups4ZAxIytWg7Q4KcVhzq0FJnZws)
 
 telegraph = Telegraph()
 telegraph.create_account(short_name='bot')
